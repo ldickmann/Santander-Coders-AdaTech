b30fe2ef3f161f75a58b96c7a29a05a4e810fe14
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-06-15 19:07:49 -0300

    atualização .gitignore e add classe Stack.java

--- Introdução--EstruturaDados/EstruturaDeDados/src/main/java/dataStructure/Stack.java
+++ Introdução--EstruturaDados/EstruturaDeDados/src/main/java/dataStructure/Stack.java
@@ -3,7 +3,12 @@ package dataStructure;
 public class Stack {
 
 
-    class Node {
+    private Node top;
+
+    private int height;
+
+
+    public class Node {
         int value;
         Node next;
 
@@ -11,7 +16,85 @@ public class Stack {
         Node(int value) {
             this.value = value;
         }
+
+        public int getValue() {
+            return value;
+        }
     }
 
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void getTop() {
+        if (top == null) {
+            System.out.println("Pilha vazia");
+        } else {
+            System.out.println("Topo: " + top.value);
+        }
+    }
+
+    public void getHeight() {
+        System.out.println("Altura: " + height);
+    }
+
+    public void print() {
+        System.out.println("###############");
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("###############");
+    }
+
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+
+    public Node pop() {
+        if (height == 0) return null;
+
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+
+        return temp;
+
+    }
+
+
+    public static void main(String[] args) {
+        Stack myStack = new Stack(2);
+        myStack.push(1);
+
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop().value);
+
+        System.out.println(myStack.pop() == null);
+
+//        myStack.getTop();
+//        myStack.getHeight();
+//
+//        myStack.print();
+
+//        myStack.push(1);
+
+//        myStack.print();
+//        myStack.getTop();
+//        myStack.getHeight();
+    }
 
 }
